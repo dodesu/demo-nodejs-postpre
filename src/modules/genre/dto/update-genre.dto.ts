645ab2b0e874384720f7aecb3248958a3e264d7c
dodesu <do204contact@gmail.com>
2025-06-24 19:35:59 +0700

    fix: rename files dto of genre

--- src/modules/genre/dto/update-genre.dto.ts
+++ src/modules/genre/dto/update-genre.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { CreateGenreDto } from "./create-genre";
+import { CreateGenreDto } from "./create-genre.dto";
 
 export class UpdateGenreDto extends PartialType(CreateGenreDto) { }
